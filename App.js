40b56a170806a23b093cf15084ba327fb7e749c4
Trace Dugar <tracedugar89@gmail.com>
2023-01-23 20:54:36 -0800

    Add Crimson Screen Light, 5 Brightness Settings

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Brightness from 'expo-brightness';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [permissionStatus, setPermissionStatus] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Brightness.requestPermissionsAsync();
+      setPermissionStatus(status);
+    })();
+  }, []);
+
+  if (permissionStatus !== "granted") {
+    return <View>
+      <Text>Permission to change brightness not granted</Text>
+    </View>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text color='#fff'>Select Brightness Level</Text>
+      <Button
+        title="Min"
+        onPress={() => Brightness.setSystemBrightnessAsync(0)}
+      />
+      <Button
+        title="Dim"
+        onPress={() => Brightness.setSystemBrightnessAsync(0.2)}
+      />
+      <Button
+        title="Mid"
+        onPress={() => Brightness.setSystemBrightnessAsync(0.5)}
+      />
+      <Button
+        title="Bright"
+        onPress={() => Brightness.setSystemBrightnessAsync(0.8)}
+      />
+      <Button
+        title="Max"
+        onPress={() => Brightness.setSystemBrightnessAsync(1)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +51,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'crimson',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Button: {
+    backgroundColor: 'red',
   },
 });
---
+++ README.md
@@ -0,0 +1,28 @@
+# good-call
+
+## Author: Trace Dugar
+
+## Collaborators
+
+## Description
+
+Tactical Screen Flashlight Phone App with a Crimson Background and 5 Settings of brightness Min - 0% , Dim - 20%, Mid - %50, Bright - 80%, Max - 100%.  Crimson was the chosen because it is the color least visible from a distance.  The Fact that the screen is the light will use less battery than the flashlight.
+
+## Links and Resources
+
+ [Github Repository](https://github.com/TraceDugar/good-call)
+ [Live Site]()
+
+### Running the application
+
+- `npm start`
+
+### Features and Routes
+
+- Feature 1: Crimson Flashlight with 5 settings.
+
+#### Tests
+
+N/A
+
+#### UML / Diagrams
